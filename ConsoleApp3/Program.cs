579145e74188b1728db5ffed4c77b4bf0f611b8f
student <student@ftn.ns>
2022-10-28 19:15:49 +0200

    Poruka

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite broj: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            while (a != 0)
+            {
+                int b = a % 10;
+                a = a / 10;
+                if (b == 3)
+                {
+                    Console.WriteLine("Broj sadrzi broj 3.");
+                    break;
+                }
+                else if (a == 0)
+                {
+                    Console.WriteLine("Broj ne sadrzi broj 3.");
+                }
+            }
+        }
+    }
+}
